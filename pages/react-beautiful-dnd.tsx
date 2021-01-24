41eabf2a2c1839108830cc98eb6f6c02bdc5a7f4
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2021-01-25 02:24:32 +0900

    indexページに戻れるようにした

--- pages/react-beautiful-dnd.tsx
+++ pages/react-beautiful-dnd.tsx
@@ -1,5 +1,13 @@
+import PageTitle from '../components/atoms/PageTitle'
+import BackToHomeButton from '../components/atoms/BackToHomeButton'
+
 const DnD = (): JSX.Element => {
-  return <div>DnD</div>
+  return (
+    <>
+      <PageTitle pageName={'react-beautiful-dnd'} />
+      <BackToHomeButton />
+    </>
+  )
 }
 
 export default DnD
