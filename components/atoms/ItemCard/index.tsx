01aeb80ec0bdfbadeea86230c8da437d1bab65ed
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2021-01-26 02:32:59 +0900

    リストにスタイル当てた

--- components/atoms/ItemCard/index.tsx
+++ components/atoms/ItemCard/index.tsx
@@ -1,7 +1,8 @@
 import { ItemType } from '../../organisms/ListContent'
+import styles from './ItemCard.module.scss'
 
 const Item = ({ item }: { item: ItemType }): JSX.Element => {
-  return <div>{item.content}</div>
+  return <div className={styles.container}>{item.content}</div>
 }
 
 export default Item
