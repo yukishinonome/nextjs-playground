c6eccc1be5f0b901421b2d25b6f3b986b3d63195
yukishinonome <yuki.yamakoshi0126@gmail.com>
2021-04-04 17:50:12 +0900

    ストーリーファイル作成

---
+++ components/organisms/Pagination/Pagination.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Pagination, { Props } from './Pagination'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Pagination',
+  component: Pagination
+} as Meta
+
+const Template: Story<Props> = (args) => <Pagination {...args} />
+
+export const Index = Template.bind({})
+Index.args = {
+  pageItems: [...Array(102)].map((_, idx) => {
+    return 'Item' + (idx + 1)
+  })
+}
