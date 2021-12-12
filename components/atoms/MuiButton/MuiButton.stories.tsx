37a464a114299360250199453ab167f5016f073a
yukishinonome <yuki.yamakoshi0126@gmail.com>
2021-12-13 01:51:24 +0900

    muiのbuttonを使ってみる

---
+++ components/atoms/MuiButton/MuiButton.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MuiButton from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Atoms/MuiButton',
+  component: MuiButton
+} as Meta
+
+const Template: Story = (args) => <MuiButton {...args} />
+
+export const Index = Template.bind({})
+Index.args = {}
---
+++ pages/mui/button.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import Layout from '../../components/Layout'
+import MuiButton from '../../components/atoms/MuiButton'
+
+const MuiButtonPage: FC = () => {
+  return (
+    <Layout pageName="MUI Button">
+      <MuiButton />
+    </Layout>
+  )
+}
+
+export default MuiButtonPage
