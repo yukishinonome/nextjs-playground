49ea64979c141eb5bc7fb2be8ae233b873684747
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-03 17:13:40 +0900

    ストーリーファイル作成

---
+++ components/organisms/DndImageList/DndImageList.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import DndImageList from './DndImageList'
+
+export default {
+  title: 'Organisms/DndImageList',
+  component: DndImageList
+} as ComponentMeta<typeof DndImageList>
+
+export const Index: ComponentStoryObj<typeof DndImageList> = {}
