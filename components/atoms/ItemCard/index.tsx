1fb5567406d37273796b96819514a8fa4dbdb071
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-07 22:05:59 +0900

    react-beautiful-dndがreact18に対応していない

--- components/atoms/ItemCard/index.tsx
+++ components/atoms/ItemCard/index.tsx
@@ -1,6 +1,6 @@
 import { ItemType } from '../../organisms/ListContent'
-import styles from './ItemCard.module.scss'
-import { Draggable } from 'react-beautiful-dnd'
+// import styles from './ItemCard.module.scss'
+// import { Draggable } from 'react-beautiful-dnd'
 
 const ItemCard = ({
   item,
@@ -10,18 +10,23 @@ const ItemCard = ({
   index: number
 }): JSX.Element => {
   return (
-    <Draggable draggableId={item.id} index={index}>
-      {(provided) => (
-        <div
-          className={styles.container}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {item.content}
-        </div>
-      )}
-    </Draggable>
+    // <Draggable draggableId={item.id} index={index}>
+    //   {(provided) => (
+    //     <div
+    //       className={styles.container}
+    //       ref={provided.innerRef}
+    //       {...provided.draggableProps}
+    //       {...provided.dragHandleProps}
+    //     >
+    //       {item.content}
+    //     </div>
+    //   )}
+    // </Draggable>
+    <>
+      <div>react 18 に対応していない</div>
+      <div>{item.id}</div>
+      <div>{index}</div>
+    </>
   )
 }
 
--- components/organisms/ListContent/ListContent.tsx
+++ components/organisms/ListContent/ListContent.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react'
 import {
-  DragDropContext,
-  Droppable,
+  // DragDropContext,
+  // Droppable,
   OnDragEndResponder
 } from 'react-beautiful-dnd'
-import ItemList from '../ItemList'
+// import ItemList from '../ItemList'
 import { ItemType } from './index'
 
 type Props = {
@@ -14,18 +14,23 @@ type Props = {
 
 const ListContent: FC<Props> = ({ onDragEnd, items }) => {
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="list">
-        {(provided) => (
-          <>
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              <ItemList items={items} />
-            </div>
-            {provided.placeholder}
-          </>
-        )}
-      </Droppable>
-    </DragDropContext>
+    // <DragDropContext onDragEnd={onDragEnd}>
+    //   <Droppable droppableId="list">
+    //     {(provided) => (
+    //       <>
+    //         <div ref={provided.innerRef} {...provided.droppableProps}>
+    //           <ItemList items={items} />
+    //         </div>
+    //         {provided.placeholder}
+    //       </>
+    //     )}
+    //   </Droppable>
+    // </DragDropContext>
+    <>
+      <div>react 18 に対応していない</div>
+      <div>{onDragEnd.length}</div>
+      <div>{items.length}</div>
+    </>
   )
 }
 
