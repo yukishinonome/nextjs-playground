d903eddbd867e7246e53edd55e36b38f7735e48b
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-10 00:37:30 +0900

    ストーリーファイルを作成

---
+++ components/atoms/ShowMoreAccordion/ShowMoreAccordion.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import ShowMoreAccordion from '.'
+
+export default {
+  title: 'Atoms/ShowMoreAccordion',
+  component: ShowMoreAccordion
+} as ComponentMeta<typeof ShowMoreAccordion>
+
+export const Index: ComponentStoryObj<typeof ShowMoreAccordion> = {}
