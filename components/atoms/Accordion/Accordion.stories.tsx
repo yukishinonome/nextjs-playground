279e38ab92548f722fd21b8d3c4099887daaac12
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-09 22:51:43 +0900

    ストーリーファイルを作成

---
+++ components/atoms/Accordion/Accordion.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import Accordion from '.'
+
+export default {
+  title: 'Atoms/Accordion',
+  component: Accordion
+} as ComponentMeta<typeof Accordion>
+
+export const Index: ComponentStoryObj<typeof Accordion> = {}
