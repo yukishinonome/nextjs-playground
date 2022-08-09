152a1e3af2fa60b408451669536608be880eeaff
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-09 22:47:12 +0900

    アコーディオンコンポーネントを作成

--- pages/ui-list.tsx
+++ pages/ui-list.tsx
@@ -1,9 +1,13 @@
+import Accordion from '@/components/atoms/Accordion'
 import { FC } from 'react'
 import Layout from '../components/Layout'
 
 const UiList: FC = () => {
   return (
     <Layout pageName="ui-list">
+      <Accordion title="アコーディオン">
+        <p>テキストテキストテキストテキストテキストテキスト</p>
+      </Accordion>
       <div>UI</div>
       <div>UI</div>
       <div>UI</div>
