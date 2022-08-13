82f771a0d63822846484bb662aaedae266115bbf
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-13 13:57:14 +0900

    RefObjectがなくてエラーになるのを防ぐ

--- components/pages/IntersectionObserverPage/index.tsx
+++ components/pages/IntersectionObserverPage/index.tsx
@@ -1,6 +1,6 @@
 import Layout from '@/components/Layout'
 import { useIntersectionObserver } from '@/hooks/use-intersection-observer'
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import styles from './IntersectionObserver.module.scss'
 
 const showElements = (entries: IntersectionObserverEntry[]) => {
@@ -12,18 +12,22 @@ const showElements = (entries: IntersectionObserverEntry[]) => {
 }
 
 const IntersectionObserverPage: FC = () => {
-  const refs = useIntersectionObserver<HTMLHeadingElement>(3, showElements)
+  const ref1 = useRef<HTMLHeadingElement>(null)
+  const ref2 = useRef<HTMLHeadingElement>(null)
+  const ref3 = useRef<HTMLHeadingElement>(null)
+
+  useIntersectionObserver<HTMLHeadingElement>([ref1, ref2, ref3], showElements)
 
   return (
     <Layout pageName="intersection-observer">
       <div className="">↓ scroll ↓</div>
-      <h2 className={styles.heading} ref={refs.current[0]}>
+      <h2 className={styles.heading} ref={ref1}>
         Hello 1
       </h2>
-      <h2 className={styles.heading} ref={refs.current[1]}>
+      <h2 className={styles.heading} ref={ref2}>
         Hello 2
       </h2>
-      <h2 className={styles.heading} ref={refs.current[2]}>
+      <h2 className={styles.heading} ref={ref3}>
         Hello 3
       </h2>
     </Layout>
--- hooks/use-intersection-observer.ts
+++ hooks/use-intersection-observer.ts
@@ -1,30 +1,22 @@
-import {
-  createRef,
-  MutableRefObject,
-  RefObject,
-  useEffect,
-  useRef
-} from 'react'
+import { RefObject, useEffect } from 'react'
 
 /**
  * @template T RefObject の型を明示する必要がある
- * @param numberOfElements 要素の数
+ * @param refs RefObject の配列
  * @param callback IntersectionObserver のインスタンス生成時に渡すコールバック関数
  * @param options IntersectionObserver のインスタンス生成時に渡すオプション
  * @return numberOfElements で指定した数の RefObject
  */
 export const useIntersectionObserver = <T extends HTMLElement>(
-  numberOfElements: number,
+  refs: RefObject<T>[],
   callback: (entries: IntersectionObserverEntry[]) => void,
   options?: IntersectionObserverInit
-): MutableRefObject<RefObject<T>[]> => {
-  const refs = useRef([...Array(numberOfElements)].map(() => createRef<T>()))
-
+): void => {
   useEffect(() => {
     const observerRefValueList: T[] = []
     const observer = new IntersectionObserver(callback, options)
 
-    refs.current.forEach((ref) => {
+    refs.forEach((ref) => {
       if (ref.current) {
         observer.observe(ref.current)
         observerRefValueList.push(ref.current)
@@ -37,6 +29,4 @@ export const useIntersectionObserver = <T extends HTMLElement>(
       })
     }
   })
-
-  return refs
 }
