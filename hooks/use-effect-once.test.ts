bda3cc45a2336c2346cd442ae3a65ca487d59977
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-20 17:02:38 +0900

    create useEffectOnce test

---
+++ hooks/use-effect-once.test.ts
@@ -0,0 +1,25 @@
+import { renderHook } from '@/test/testUtils'
+import { useEffectOnce } from './use-effect-once'
+
+const mockEffectCleanup = jest.fn()
+const mockEffectCallback = jest.fn().mockReturnValue(mockEffectCleanup)
+
+it('should run provided effect only once', () => {
+  const { rerender } = renderHook(() => useEffectOnce(mockEffectCallback))
+
+  expect(mockEffectCallback).toHaveBeenCalledTimes(1)
+
+  rerender()
+
+  expect(mockEffectCallback).toHaveBeenCalledTimes(1)
+})
+
+it('should run clean-up provided on unmount', () => {
+  const { unmount } = renderHook(() => useEffectOnce(mockEffectCallback))
+
+  expect(mockEffectCleanup).not.toHaveBeenCalled()
+
+  unmount()
+
+  expect(mockEffectCleanup).toHaveBeenCalledTimes(1)
+})
