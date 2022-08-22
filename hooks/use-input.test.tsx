4497fd090f73e2282405224713662e7bb5936f99
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-23 01:02:39 +0900

    add @testing-library/user-event

--- hooks/use-input.test.tsx
+++ hooks/use-input.test.tsx
@@ -1,10 +1,11 @@
-import { act, fireEvent, render, renderHook } from '@/test/testUtils'
+import { renderHook, screen, setup } from '@/test/testUtils'
 import { useInput } from './use-input'
 
-const setUp = (initialValue: string) => renderHook(() => useInput(initialValue))
+const setUpHook = (initialValue: string) =>
+  renderHook(() => useInput(initialValue))
 
-it('期待する戻り値である', () => {
-  const { result } = setUp('hoge')
+test('期待する戻り値である', () => {
+  const { result } = setUpHook('hoge')
 
   expect(result.current.length).toBe(2)
   expect(result.current[0].value).toBe('hoge')
@@ -12,29 +13,22 @@ it('期待する戻り値である', () => {
   expect(typeof result.current[1]).toBe('function')
 })
 
-it('入力した値で更新されて、戻り値の関数で更新された値を初期化できる', () => {
-  const { result } = setUp('')
+test('入力した値で更新されて、戻り値の関数で更新された値を初期化できる', async () => {
+  const { result } = setUpHook('')
   const [form1Props, resetFormValue] = result.current
 
-  const { container } = render(
+  const { user } = setup(
     <>
       <input type="text" placeholder="form" required {...form1Props} />
       <button onClick={resetFormValue}>reset</button>
     </>
   )
 
-  const input = container.querySelector('input')
-  const button = container.querySelector('button')
-
-  act(() => {
-    fireEvent.change(input!, { target: { value: 'hello' } })
-  })
+  await user.type(screen.getByRole('textbox'), 'hello')
 
   expect(result.current[0].value).toBe('hello')
 
-  act(() => {
-    fireEvent.click(button!)
-  })
+  await user.click(screen.getByRole('button'))
 
   expect(result.current[0].value).toBe('')
 })
