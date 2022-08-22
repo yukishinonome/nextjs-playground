75e24bc8c521269c4a43d53331fda3ca459769b5
yukishinonome <yuki.yamakoshi0126@gmail.com>
2022-08-23 01:02:54 +0900

    Revert "add @testing-library/user-event" userEvent.typeが最後の1文字しか返さない謎挙動
    This reverts commit 4497fd090f73e2282405224713662e7bb5936f99.

--- hooks/use-input.test.tsx
+++ hooks/use-input.test.tsx
@@ -1,11 +1,10 @@
-import { renderHook, screen, setup } from '@/test/testUtils'
+import { act, fireEvent, render, renderHook } from '@/test/testUtils'
 import { useInput } from './use-input'
 
-const setUpHook = (initialValue: string) =>
-  renderHook(() => useInput(initialValue))
+const setUp = (initialValue: string) => renderHook(() => useInput(initialValue))
 
-test('期待する戻り値である', () => {
-  const { result } = setUpHook('hoge')
+it('期待する戻り値である', () => {
+  const { result } = setUp('hoge')
 
   expect(result.current.length).toBe(2)
   expect(result.current[0].value).toBe('hoge')
@@ -13,22 +12,29 @@ test('期待する戻り値である', () => {
   expect(typeof result.current[1]).toBe('function')
 })
 
-test('入力した値で更新されて、戻り値の関数で更新された値を初期化できる', async () => {
-  const { result } = setUpHook('')
+it('入力した値で更新されて、戻り値の関数で更新された値を初期化できる', () => {
+  const { result } = setUp('')
   const [form1Props, resetFormValue] = result.current
 
-  const { user } = setup(
+  const { container } = render(
     <>
       <input type="text" placeholder="form" required {...form1Props} />
       <button onClick={resetFormValue}>reset</button>
     </>
   )
 
-  await user.type(screen.getByRole('textbox'), 'hello')
+  const input = container.querySelector('input')
+  const button = container.querySelector('button')
+
+  act(() => {
+    fireEvent.change(input!, { target: { value: 'hello' } })
+  })
 
   expect(result.current[0].value).toBe('hello')
 
-  await user.click(screen.getByRole('button'))
+  act(() => {
+    fireEvent.click(button!)
+  })
 
   expect(result.current[0].value).toBe('')
 })
--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { ReactElement } from 'react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
@@ -19,16 +18,7 @@ const Providers = ({ children }: { children: ReactElement }) => {
 const customRender = (ui: ReactElement, options = {}) =>
   render(ui, { wrapper: Providers, ...options })
 
-const setup = (jsx: JSX.Element) => {
-  return {
-    user: userEvent.setup(),
-    ...customRender(jsx)
-  }
-}
-
 // re-export everything
 export * from '@testing-library/react'
 // override render method
 export { customRender as render }
-// export user-event
-export { userEvent, setup }
